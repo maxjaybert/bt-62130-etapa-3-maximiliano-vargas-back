b5afb072bda540c74121ab364979b1366501d5cf
Maximiliano Vargas <vargas.maxi@gmail.com>
2023-11-28 23:25:05 -0300

    agrego back

---
+++ db.productos.js
@@ -0,0 +1,141 @@
+db.productos.insertMany(
+    [
+        {
+            "id": 1,
+            "nombre": "Airpods 3ra Gen",
+            "foto": "img/airpods-3.webp",
+            "detalles": "Incluye cargador cable USB C y una funda de regalo a eleccion",
+            "precio": "u$d 254",
+            "stock": "4",
+            "marca": "Apple",
+            "categoria": "Auriculares",
+            "envio": true,
+            "cantidad": 12
+        },
+        {
+            "id": 2,
+            "nombre": "Beats Studio 3 - Gold",
+            "foto": "img/beats-studio-3-gold.webp",
+            "detalles": "Incluye estuche dorado Beats Studio original y cargador wirless",
+            "precio": "u$d 500",
+            "stock": "4",
+            "marca": "Beats",
+            "categoria": "Auriculares",
+            "envio": true,
+            "cantidad": 5
+        },
+        {
+            "id": 3,
+            "nombre": "Beats Studio 3 - White",
+            "foto": "img/beats-studio-3-white.webp",
+            "detalles": "Incluye estuche Beats Studio original (color a elección) y cargador wirless",
+            "precio": "u$d 500",
+            "stock": "4",
+            "marca": "Beats",
+            "categoria": "Auriculares",
+            "envio": true,
+            "cantidad": 7
+        },
+        {
+            "id": 4,
+            "nombre": "Bose Soundlink",
+            "foto": "img/bose-soundlink-mini.webp",
+            "detalles": "El altavoz Bose Soundlink utiliza el sistema True Stereophonic, un auténtico sonido de 360° que te permitirá experimentar desde cualquier punto la música como a ti te gusta.",
+            "precio": "u$d 1050",
+            "stock": "4",
+            "marca": "Bose",
+            "categoria": "Auriculares",
+            "envio": true,
+            "cantidad": 6
+        },
+        {
+            "id": 5,
+            "nombre": "Bose Soundlink Mini",
+            "foto": "img/bose-soundlink.webp",
+            "detalles": "El altavoz Bose Soundlink Mini está diseñado para ir contigo a todas partes y envolverte en un sonido potente y supernítido.",
+            "precio": "u$d 950",
+            "stock": "4",
+            "marca": "Bose",
+            "categoria": "Auriculares",
+            "envio": true,
+            "cantidad": 10
+        },
+        {
+            "id": 6,
+            "nombre": "Apple Watch SE",
+            "foto": "img/applewatch-se.webp",
+            "detalles": "Una gran pantalla Retina para que tengas más información de un solo vistazo. Sensores avanzados que registran todas tus formas de moverte.",
+            "precio": "u$d 400",
+            "stock": "4",
+            "marca": "Apple",
+            "categoria": "Smartwatch",
+            "envio": true,
+            "cantidad": 20
+        },
+        {
+            "id": 7,
+            "nombre": "Ipad Pro 10 | 11 | 12 Gen",
+            "foto": "img/ipad-1.webp",
+            "detalles": "Incluye cargador cable USB C y vidrio templado Gorila Glass de regalo",
+            "precio": "desde u$d 700",
+            "stock": "4",
+            "marca": "Apple",
+            "categoria": "Ipad",
+            "envio": true,
+            "cantidad": 50
+        },
+        {
+            "id": 8,
+            "nombre": "Iphone 13 | 13 Pro | 13 Pro Max",
+            "foto": "img/iphone-13.webp",
+            "detalles": "Incluye cargador cable USB C y una funda de regalo a eleccion",
+            "precio": "desde u$d 900",
+            "stock": "4",
+            "marca": "Apple",
+            "categoria": "Iphone",
+            "envio": true,
+            "cantidad": 30
+        },
+        {
+            "id": 9,
+            "nombre": "Iphone 14 | 14 Pro | 14 Pro Max",
+            "foto": "img/iphone-14-pro.webp",
+            "detalles": "Incluye cargador cable USB C y vidrio templado Gorila Glass de regalo",
+            "precio": "desde u$d 1000",
+            "stock": "4",
+            "marca": "Apple",
+            "categoria": "Iphone",
+            "envio": true,
+            "cantidad": 30
+        },
+        {
+            "id": 10,
+            "nombre": "Samsung Galaxy Fold 3",
+            "foto": "img/galaxy-fold-3.webp",
+            "detalles": "Incluye cargador cable USB C",
+            "precio": "u$d 1200",
+            "stock": "4",
+            "marca": "Samsung",
+            "categoria": "Galaxy-Fold",
+            "envio": true,
+            "cantidad": 25
+        },
+        {
+            "id": 11,
+            "nombre": "Samsung Galaxy Z Flip 5",
+            "foto": "img/galaxy-z-flip-5.webp",
+            "detalles": "Incluye cargador cable USB C",
+            "precio": "u$d 800",
+            "stock": "4",
+            "marca": "Samsung",
+            "categoria": "Galaxy-Flip",
+
+            "envio": true,
+            "cantidad": 20
+        }
+
+    ]
+
+
+
+)
